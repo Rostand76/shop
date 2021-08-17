1b1fa1c796fc1498f2f66a051d42fb6176c833eb
Rostand76 <nguemowamba@gmail.com>
2021-08-17 14:55:57 +0100

    Add all my files

---
+++ header-for-template.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="col-sm-2">
+    <img class="img-fluid float-left" src="logo.jpg" alt="Logo">
+</div>
+   
+<div class="col-sm-8">
+   <h1 class="font-bold">Header Goes Here</h1>
+    <nav class="col-sm-2">
+         <div class="btn-group-vertical btn-group-sm" role="group" aria-label="Button Group">
+  <button type="button" class="btn btn-secondary" onclick="location.href = 'register-page.php'" >
+                   Register</button>
+  <button type="button" class="btn btn-secondary" onclick="location.href = 'register-view_users-page.php'">
+                  View Users</button>
+<button type="button" class="btn btn-secondary" onclick="location.href = 'register-password.php'" >
+                  New Password</button>
+</div>
+    </nav>
+</div>
+    </body>
+</html>
